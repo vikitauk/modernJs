9ce320af67598779dc47f6ee2a38df12ca7a1f9e
martin <artoma@gmail.com>
2017-10-07 11:57:51 +0300

    ECMA Script 6 .md file

--- playground.js
+++ playground.js
@@ -1,10 +1,1 @@
 'use strict';
-
-function multiplay(multiplier, ...theArgs){
-    return theArgs.map( function (element) {
-        return element*multiplier;
-    })
-}
-
-var arr = multiplay(2,1,2,3)
-console.log(arr);
