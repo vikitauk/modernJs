c3d8dbe4bc54e18f5333e64468d83b48f51caa88
martin <artoma@gmail.com>
2018-03-26 11:14:38 +0300

    ReactJS, Hello World APP - setState()

--- playground.js
+++ playground.js
@@ -1,7 +1,26 @@
-"use strict";
+let person = {
+    name: 'Ivan',
+    gender: 'male',
+    age: '25',
+    address: {
+        country: 'Bulgaria',
+        city: 'Sofia',
+        str: 'Rakovska',
+    }
+}
 
-console.log("Hi");
+// attach Symbol.iterator
+person[Symbol.iterator] = function (){
+    for(const key in this){
 
-setTimeout( function () {
-    console.log("callback hi");
-}, 6000);
+        return key; // key name
+        //yield this[key]; // key value
+        //yield `${key}: ${this[key]}`;// key value
+    }
+}
+
+
+//console.log(person.next())
+for(let x of person){
+  console.log(x)
+}
